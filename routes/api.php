56de394fef51501940138047cdd9e49ce9522d36
Md. Imran Hossain <mdimran1409036@gmail.com>
2021-04-05 12:41:59 +0600

    santum setup done and token added while register and login, token destroyed while logout and route are protetced using auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
 /*
@@ -16,10 +17,29 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//proteccted routes
+
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
-Route::resource('/products', ProductController:: class);
+
+//public routes
+Route::post('/register', [AuthController:: class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+
+// Route::resource('/products', ProductController::class);
